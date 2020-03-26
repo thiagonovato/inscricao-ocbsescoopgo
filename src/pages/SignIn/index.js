894fe8c2733da19a9ee1131447f6f895d79c8ef3
Thiago Novato <thiagonovato@gmail.com>
2020-03-26 15:24:38 -0300

    Configuração e validação da tela de login.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 
-// import { Container } from './styles';
+import logoOcb from "../../assets/logo-ocb-menor.png";
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um e-mail válido.")
+    .required("O e-mail é obrigatório."),
+  password: Yup.string().required("A senha é obrigatória.")
+});
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  function handlerSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logoOcb} alt="Covid19" />
+
+      <Form schema={schema} onSubmit={handlerSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Acessar</button>
+        <Link to="/">Voltar ao Formulário</Link>
+      </Form>
+    </>
+  );
 }
