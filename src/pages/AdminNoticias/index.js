61f3e04d711e5a00a4f258c7f252bd46daeefced
Thiago Novato <thiagonovato@gmail.com>
2020-03-26 14:33:47 -0300

    Primeiro commit.

---
+++ src/pages/AdminNoticias/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function AdminNoticias() {
+  return <h1>Administração de Notícias</h1>;
+}
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+import Route from "./Route";
+
+// Importando as páginas
+import Form from "../pages/Form";
+import SignIn from "../pages/SignIn";
+import Noticias from "../pages/AdminNoticias";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Form} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/adm/noticias" component={Noticias} isPrivate />
+        <Route path="/" component={() => <h1>404</h1>} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
