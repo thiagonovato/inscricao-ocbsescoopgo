4085f3c802c7bc08c8ea892144b3e3667b61d3c4
Thiago Novato <thiagonovato@gmail.com>
2020-03-27 16:13:16 -0300

    Corrigido as rotas.'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, BrowserRouter as Router } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import Route from "./Route";
 
 // Importando as páginas
@@ -9,13 +9,11 @@ import Noticias from "../pages/AdminNoticias";
 
 export default function Routes() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/signin" component={SignIn} auth />
-        <Route path="/adm/noticias" component={Noticias} isPrivate />
-        <Route path="/" component={() => <h1>404</h1>} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/signin" component={SignIn} auth />
+      <Route path="/adm/noticias" component={Noticias} isPrivate />
+      <Route path="/" component={() => <h1>404</h1>} />
+    </Switch>
   );
 }
