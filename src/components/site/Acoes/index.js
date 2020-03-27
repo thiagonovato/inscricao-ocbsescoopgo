2089f5d14b207acfc2963024ae6affdf3eef2bea
Thiago Novato <thiagonovato@gmail.com>
2020-03-27 09:37:47 -0300

    Colocando as notícias.

--- src/components/site/Acoes/index.js
+++ src/components/site/Acoes/index.js
@@ -1,15 +1,47 @@
 import React from "react";
 
-import { Container } from "./styles";
+import logo from "../../../assets/logo-covid.png";
+
+import { Container, Titulo, Noticia } from "./styles";
 
 export default function Acoes() {
   return (
     <Container>
-      <div>COOPERATIVISMO GOIANO CONTRA O CORONA-VÍRUS</div>
-      <span>
-        Veja aqui todas as ações do Sistema OCB/SESCOOP-GO e das cooperativas
-        goianas frente à crise gerada pelo COVID-19 (coronavírus)
-      </span>
+      <Titulo>
+        <div>COOPERATIVISMO GOIANO CONTRA O CORONA-VÍRUS</div>
+        <span>
+          Veja aqui todas as ações do Sistema OCB/SESCOOP-GO e das cooperativas
+          goianas frente à crise gerada pelo COVID-19 (coronavírus)
+        </span>
+      </Titulo>
+      <Noticia>
+        <img src={logo} alt="Noticia" />
+        <div>
+          <titulo>Aqui é o título</titulo>
+          <chamada>Aqui é o copor da notícia</chamada>
+        </div>
+      </Noticia>
+      <Noticia>
+        <img src={logo} alt="Noticia" />
+        <div>
+          <titulo>Aqui é o título</titulo>
+          <chamada>Aqui é o copor da notícia</chamada>
+        </div>
+      </Noticia>
+      <Noticia>
+        <img src={logo} alt="Noticia" />
+        <div>
+          <titulo>Aqui é o título</titulo>
+          <chamada>Aqui é o copor da notícia</chamada>
+        </div>
+      </Noticia>
+      <Noticia>
+        <img src={logo} alt="Noticia" />
+        <div>
+          <titulo>Aqui é o título</titulo>
+          <chamada>Aqui é o copor da notícia</chamada>
+        </div>
+      </Noticia>
     </Container>
   );
 }
--- src/components/site/Acoes/styles.js
+++ src/components/site/Acoes/styles.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  padding: 0 30px 0 30px;
+export const Container = styled.div``;
+
+export const Titulo = styled.div`
+  padding: 0 30px 30px 30px;
   text-align: center;
 
   div {
@@ -13,5 +15,31 @@ export const Container = styled.div`
 
   span {
     font-size: 16px;
+  }
+`;
+
+export const Noticia = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 50px 30px 50px;
+
+  img {
+    height: 150px;
+    border-radius: 50px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    titulo {
+      font-size: 22px;
+      font-weight: bold;
+      padding-bottom: 10px;
+    }
+
+    chamada {
+      font-size: 16px;
+    }
   }
 `;
